dd3f15b50cebf154117ee6e72497e980a92ef18c
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-17 23:17:12 +0100

    add Balance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Balance from './Balance';
 
 const Header = () => {
-    return <h2>Expanse Tracker</h2>;
+    return (
+        <>
+            <h2>Expanse Tracker</h2>
+            <div className='container'>
+                <Balance />
+            </div>
+        </>
+    );
 };
 
 export default Header;
