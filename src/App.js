2226e9bc4706e0278eb26e1df20680be4c625c04
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-18 19:20:27 +0100

    add TransactionList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import TransactionList from './components/TransactionList';
 
 function App() {
     return (
         <div>
             <Header />
+            <TransactionList />
         </div>
     );
 }
