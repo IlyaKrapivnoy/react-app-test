05649f5cfed7a41dd47457b9b8ea72a5b6760fb4
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-19 22:56:03 +0100

    add context

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
     return (
         <div>
             <Header />
+            <IncomeExpenses />
             <TransactionList />
             <AddTransaction />
         </div>
