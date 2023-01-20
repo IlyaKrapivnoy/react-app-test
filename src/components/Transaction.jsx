ff8cd22b1e9b6d979fd96d0a785d7f86b46e37d8
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-20 20:11:26 +0100

    make border dynamic

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,7 +4,7 @@ const Transaction = ({transaction}) => {
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
-        <li className='minus'>
+        <li className={sign === '+' ? 'plus' : 'minus'}>
             {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
             <button className='delete-btn'>x</button>
         </li>
