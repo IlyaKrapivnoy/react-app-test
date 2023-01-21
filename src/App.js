2c8ff6288b6d313ad01af10c12991df9f8e59ec1
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-01-21 12:44:35 +0100

    add annual amount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import './App.css';
-import Header from './components/Header';
-import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
-import { GlobalProvider } from './context/GlobalState';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
-import React from 'react';
-import ChurchTax from './components/ChurchTax';
+import "./App.css";
+import Header from "./components/Header";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import React from "react";
+import ChurchTax from "./components/ChurchTax";
+import AnnualAmount from "./components/AnnualAmount";
 
 function App() {
-    return (
-        <GlobalProvider>
-            <Header />
-            <div className='container'>
-                <Balance />
-                <IncomeExpenses />
-                <ChurchTax />
-                <TransactionList />
-                <AddTransaction />
-            </div>
-        </GlobalProvider>
-    );
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <div className="counts-field">
+          <ChurchTax />
+          <AnnualAmount />
+        </div>
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
 }
 
 export default App;
